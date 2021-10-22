4f6b63cf02c7146a6bb414ffc54f3c9e003dcbed
Artur Przychodzen <artur2p@gmail.com>
2021-10-22 12:04:38 +0200

    initial

---
+++ src/main/java/io/artur/spring/usingspringstatemachine/UsingSpringStateMachineApplication.java
@@ -0,0 +1,13 @@
+package io.artur.spring.usingspringstatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingSpringStateMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingSpringStateMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/artur/spring/usingspringstatemachine/UsingSpringStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.artur.spring.usingspringstatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingSpringStateMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
